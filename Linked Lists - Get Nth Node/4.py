731d90b814c755550757c5d94d4413744ba22577
alinabodnarpn <bodnar.pn@ucu.edu.ua>
2025-04-03 12:58:02 +0300

    Add files via upload
    first upload of the 4 task, Invalid index error

---
+++ Linked Lists - Get Nth Node/4.py
@@ -0,0 +1,16 @@
+class Node():
+    def __init__(self, data, next = None):
+        self.data = data
+        self.next = next
+
+
+def get_nth(node, index):
+    if node is None:
+        raise IndexError("Index out of range")
+    current = node
+    count = 0
+    while current is not None:
+        if count == index:
+            return current
+        current = current.next
+        count += 1
